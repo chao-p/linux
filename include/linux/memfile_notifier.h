4c0aa08b151cb949b0f2735df1fe20935d2de79e
Chao Peng <chao.p.peng@linux.intel.com>
2022-01-12 13:39:41 +0800

    mm: Introduce memfile_notifier
    This patch introduces memfile_notifier facility so existing memory file
    subsystems (e.g. tmpfs/hugetlbfs) can provide memory pages to allow a
    third kernel component to make use of memory bookmarked in the memory
    file and gets notified when the pages in the memory file become
    allocated/invalidated.
    
    It will be used for KVM to use a file descriptor as the guest memory
    backing store and KVM will use this memfile_notifier interface to
    interact with memory file subsystems. In the future there might be other
    consumers (e.g. VFIO with encrypted device memory).
    
    It consists two sets of callbacks:
      - memfile_notifier_ops: callbacks for memory backing store to notify
        KVM when memory gets allocated/invalidated.
      - memfile_pfn_ops: callbacks for KVM to call into memory backing store
        to request memory pages for guest private memory.
    
    Userspace is in charge of guest memory lifecycle: it first allocates
    pages in memory backing store and then passes the fd to KVM and lets KVM
    register each memory slot to memory backing store via
    memfile_register_notifier.
    
    The supported memory backing store should maintain a memfile_notifier list
    and provide routine for memfile_notifier to get the list head address and
    memfile_pfn_ops callbacks for memfile_register_notifier. It also should call
    memfile_notifier_fallocate/memfile_notifier_invalidate when the bookmarked
    memory gets allocated/invalidated.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

---
+++ include/linux/memfile_notifier.h
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_MEMFILE_NOTIFIER_H
+#define _LINUX_MEMFILE_NOTIFIER_H
+
+#include <linux/rculist.h>
+#include <linux/spinlock.h>
+#include <linux/srcu.h>
+#include <linux/fs.h>
+
+struct memfile_notifier;
+
+struct memfile_notifier_ops {
+	void (*invalidate)(struct memfile_notifier *notifier,
+			   pgoff_t start, pgoff_t end);
+	void (*fallocate)(struct memfile_notifier *notifier,
+			  pgoff_t start, pgoff_t end);
+};
+
+struct memfile_pfn_ops {
+	long (*get_lock_pfn)(struct inode *inode, pgoff_t offset, int *order);
+	void (*put_unlock_pfn)(unsigned long pfn);
+};
+
+struct memfile_notifier {
+	struct list_head list;
+	struct memfile_notifier_ops *ops;
+};
+
+struct memfile_notifier_list {
+	struct list_head head;
+	spinlock_t lock;
+};
+
+#ifdef CONFIG_MEMFILE_NOTIFIER
+static inline void memfile_notifier_list_init(struct memfile_notifier_list *list)
+{
+	INIT_LIST_HEAD(&list->head);
+	spin_lock_init(&list->lock);
+}
+
+extern void memfile_notifier_invalidate(struct memfile_notifier_list *list,
+					pgoff_t start, pgoff_t end);
+extern void memfile_notifier_fallocate(struct memfile_notifier_list *list,
+				       pgoff_t start, pgoff_t end);
+extern int memfile_register_notifier(struct inode *inode,
+				     struct memfile_notifier *notifier,
+				     struct memfile_pfn_ops **pfn_ops);
+extern void memfile_unregister_notifier(struct inode *inode,
+					struct memfile_notifier *notifier);
+
+#endif /* CONFIG_MEMFILE_NOTIFIER */
+
+#endif /* _LINUX_MEMFILE_NOTIFIER_H */
