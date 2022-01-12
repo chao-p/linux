849f07cb29e8de467d7693f6eca011daf170aa22
Chao Peng <chao.p.peng@linux.intel.com>
2022-01-12 13:39:41 +0800

    mm: Introduce memfile_notifier
    This patch introduces memfile_notifier facility so existing memory file
    subsystems (e.g. tmpfs/hugetlbfs) can provide memory pages to allow a
    third kernel component to make use of memory bookmarked in the memory
    file and gets notified when the pages in the memory file become
    allocated/invalidated.
    
    It will be used for KVM to use a file descriptor as the guest memory
    backing store and KVM will use this memfile_notifier interface to
    interact with memory file subsystems. In the future there might be other
    consumers (e.g. VFIO with encrypted device memory).
    
    It consists two sets of callbacks:
      - memfile_notifier_ops: callbacks for memory backing store to notify
        KVM when memory gets allocated/invalidated.
      - memfile_pfn_ops: callbacks for KVM to call into memory backing store
        to request memory pages for guest private memory.
    
    Userspace is in charge of guest memory lifecycle: it first allocates
    pages in memory backing store and then passes the fd to KVM and lets KVM
    register each memory slot to memory backing store via
    memfile_register_notifier.
    
    The supported memory backing store should maintain a memfile_notifier list
    and provide routine for memfile_notifier to get the list head address and
    memfile_pfn_ops callbacks for memfile_register_notifier. It also should call
    memfile_notifier_fallocate/memfile_notifier_invalidate when the bookmarked
    memory gets allocated/invalidated.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

---
+++ mm/memfile_notifier.c
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  linux/mm/memfile_notifier.c
+ *
+ *  Copyright (C) 2022  Intel Corporation.
+ *             Chao Peng <chao.p.peng@linux.intel.com>
+ */
+
+#include <linux/memfile_notifier.h>
+#include <linux/srcu.h>
+
+DEFINE_STATIC_SRCU(srcu);
+
+void memfile_notifier_invalidate(struct memfile_notifier_list *list,
+				 pgoff_t start, pgoff_t end)
+{
+	struct memfile_notifier *notifier;
+	int id;
+
+	id = srcu_read_lock(&srcu);
+	list_for_each_entry_srcu(notifier, &list->head, list,
+				 srcu_read_lock_held(&srcu)) {
+		if (notifier->ops && notifier->ops->invalidate)
+			notifier->ops->invalidate(notifier, start, end);
+	}
+	srcu_read_unlock(&srcu, id);
+}
+
+void memfile_notifier_fallocate(struct memfile_notifier_list *list,
+				pgoff_t start, pgoff_t end)
+{
+	struct memfile_notifier *notifier;
+	int id;
+
+	id = srcu_read_lock(&srcu);
+	list_for_each_entry_srcu(notifier, &list->head, list,
+				 srcu_read_lock_held(&srcu)) {
+		if (notifier->ops && notifier->ops->fallocate)
+			notifier->ops->fallocate(notifier, start, end);
+	}
+	srcu_read_unlock(&srcu, id);
+}
+
+static int memfile_get_notifier_info(struct inode *inode,
+				     struct memfile_notifier_list **list,
+				     struct memfile_pfn_ops **ops)
+{
+	return -EOPNOTSUPP;
+}
+
+int memfile_register_notifier(struct inode *inode,
+			      struct memfile_notifier *notifier,
+			      struct memfile_pfn_ops **pfn_ops)
+{
+	struct memfile_notifier_list *list;
+	int ret;
+
+	if (!inode || !notifier | !pfn_ops)
+		return -EINVAL;
+
+	ret = memfile_get_notifier_info(inode, &list, pfn_ops);
+	if (ret)
+		return ret;
+
+	spin_lock(&list->lock);
+	list_add_rcu(&notifier->list, &list->head);
+	spin_unlock(&list->lock);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(memfile_register_notifier);
+
+void memfile_unregister_notifier(struct inode *inode,
+				 struct memfile_notifier *notifier)
+{
+	struct memfile_notifier_list *list;
+
+	if (!inode || !notifier)
+		return;
+
+	BUG_ON(memfile_get_notifier_info(inode, &list, NULL));
+
+	spin_lock(&list->lock);
+	list_del_rcu(&notifier->list);
+	spin_unlock(&list->lock);
+
+	synchronize_srcu(&srcu);
+}
+EXPORT_SYMBOL_GPL(memfile_unregister_notifier);
